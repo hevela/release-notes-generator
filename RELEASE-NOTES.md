7d7e8e3f709c59bb6424ab7a11656f56a682040f
Hector Vela <hector.vela@wizeline.com>
2022-02-24 12:54:36 -0600

    release notes generator

---
+++ RELEASE-NOTES.md
@@ -0,0 +1,150 @@
+# cpe-sportsclip
+
+## 1.51.0 (2021-11-17)
+
+#### Feature
+
+* [MINT-1088] add apigateway file (a7fac562)
+
+#### Bug Fixes
+
+* migrated to use images for aws ecr --skip-ci (aefb23ed)
+* migrated to use images for aws ecr --skip-ci (b5c0ff38)
+
+
+
+## 1.50.0 (2021-09-02)
+
+#### Feature
+
+* [WAR-963] turn availableDate into time pointer to prevent default value (cdae74a2)
+
+
+
+# cpe-sportsclip-worker
+
+## 1.27.3 (2022-02-17)
+
+#### Bug Fixes
+
+* [MINT-1721] remove return (#67) (8a9027fe)
+
+
+
+## 1.27.2 (2022-02-17)
+
+#### Bug Fixes
+
+* [MINT-1721] use sportsEventURI as gameID if sportsEventURI is passed (#66) (1ad78d75)
+
+
+
+## 1.27.1 (2022-02-16)
+
+#### Bug Fixes
+
+* [MINT-1125] retrigger deployment (#65) (dbf6a01c)
+
+
+
+## 1.27.0 (2022-02-11)
+
+#### Feature
+
+* [MINT-1721] propagate sports event URI received from clips processor (#63) (7a0f982a)
+
+
+
+## 1.26.0 (2022-01-14)
+
+#### codeship
+
+* update builder version (84bf626c)
+
+#### executor
+
+* simplify patchMSVideoValues and use on MSClip (fca7c5dd)
+
+#### Feature
+
+* [MINT-1125] conditional tags append (8fd9800b)
+
+#### Bug Fixes
+
+* migrated to use images for aws ecr --skip-ci (0fb2d323)
+
+
+
+## 1.25.1 (2021-09-09)
+
+#### bugfix
+
+* [WAR-986] talents by showcode being overwritten by talent by name (#55) (48f959e1)
+
+#### Bug Fixes
+
+* migrated to use images for aws ecr --skip-ci (20bc9852)
+
+
+
+# cpe-ms-clips-processor
+
+## 1.15.0 (2022-02-11)
+
+#### Feature
+
+* [MINT-1721] add sportsEventURI in SNS message (#35) (0bc5ca04)
+
+
+
+## 1.14.0 (2022-01-20)
+
+#### Chores
+
+* **asset v2:** added logs (b0446bba)
+* renamed assetv1 artifacts (ce93d996)
+
+#### Feature
+
+* added asset v2 call (dcc14467)
+
+
+
+## 1.13.0 (2022-01-14)
+
+#### Feature
+
+* [MINT-1125] pass-by MS tags (32c46cb8)
+
+
+
+## 1.12.1 (2021-12-13)
+
+#### Bug Fixes
+
+* [MINT-1592] take the ExternalSource root update as latest (071b2837)
+
+
+
+## 1.12.0 (2021-10-21)
+
+#### Feature
+
+* [WAR-981] add linkedHouseID structure (#30) (b35b3dac)
+
+#### Bug Fixes
+
+* migrated to use images for aws ecr --skip-ci (fbfe24b7)
+* migrated to use images for aws ecr --skip-ci (b83a0e6f)
+* migrated to use images for aws ecr --skip-ci (26f5c301)
+
+
+
+## 1.11.0 (2021-09-02)
+
+#### Feature
+
+* [WAR-960] [MS] Standardize Unix timestamps handling (b92d3f28)
+
+
+
---
+++ main.py
@@ -0,0 +1,78 @@
+import sys
+import getopt
+import datetime
+from typing import List, Dict, Any
+from ghapi.all import GhApi
+from decouple import config
+
+USAGE = f"Usage: python {sys.argv[0]} " \
+        f"[--help] | [-o <owner>] [-r <repositories,as,csv>] [-s <start YYYY/MM/DD>] [-e <end YYYY/MM/DD>]"
+GH_TOKEN = config("GITHUB_TOKEN")
+
+
+def parse(args: List[str]) -> Dict[str, Any]:
+    options, _ = getopt.getopt(
+        args,
+        "o:r:s:e",
+        ["owner=", "repositories=", "start=", "end"]
+    )
+    args = dict()
+    for o, v in options:
+        if o in ("-h", "--help"):
+            print(USAGE)
+            sys.exit()
+        if o in ("-o", "--owner"):
+            args["owner"] = v
+        if o in ("-r", "--repositories"):
+            repos = v.split(",")
+            args["repositories"] = []
+            for repo in repos:
+                args["repositories"].append(repo)
+        if o in ("-s", "--start"):
+            date = datetime.datetime.strptime(v, "%Y/%m/%d")
+            args["start"] = date
+        if o in ("-e", "--end"):
+            date = datetime.datetime.strptime(v, "%Y/%m/%d")
+            args["end"] = date
+    if "end" not in args.keys():
+        args["end"] = datetime.datetime.now()
+    return args
+
+
+def get_releases(args: Dict[str, Any]):
+    owner = args["owner"]
+    release_notes = dict()
+    for repo in args["repositories"]:
+        release_notes[repo] = []
+        api = GhApi(owner=owner, repo=repo, token=GH_TOKEN)
+        resp = api.repos.list_releases()
+        #  published_at: 2021-11-17T21:30:45Z
+        for release in resp:
+            published_at = datetime.datetime.strptime(release.published_at, "%Y-%m-%dT%H:%M:%SZ")
+            if args["end"] > published_at > args["start"]:
+                release_notes[repo].append(release.body)
+    with open("RELEASE-NOTES.md", "w") as f:
+        for repo in release_notes.keys():
+            f.write(f"# {repo}\n\n")
+            for release in release_notes[repo]:
+                f.write(f"{release}\n\n")
+
+    with open("RELEASE-NOTES.md", "r") as f:
+        data = f.read()
+    start = args['start'].strftime('%Y-%m-%d')
+    end = args['end'].strftime('%Y-%m-%d')
+    description = f"Release notes for {start} - {end}"
+    api = GhApi(token=GH_TOKEN)
+    api.gists.create(description=description, files={f'RELEASE_NOTES_{end}.md': dict(content=data)}, public=False)
+
+
+def main():
+    args = sys.argv[1:]
+    if not args:
+        raise SystemExit(USAGE)
+    args = parse(args)
+    get_releases(args)
+
+
+if __name__ == '__main__':
+    main()
